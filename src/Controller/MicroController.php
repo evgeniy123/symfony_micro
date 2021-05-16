7ecc1f41734b914343aea0eebdfa42271ba0e7f5
Tarasevich Yauheni <evgeniy123@gmail.com>
2021-05-16 22:06:50 +0200

    first commit

---
+++ src/Controller/MicroController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class MicroController extends AbstractController
+{
+    /**
+     * @Route("/random/{limit}")
+     */
+    public function randomNumber(int $limit): Response
+    {
+        $number = random_int(0, $limit);
+
+        return $this->render('micro/random.html.twig', [
+            'number' => $number,
+        ]);
+    }
+}
